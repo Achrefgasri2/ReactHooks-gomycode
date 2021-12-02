8aa4910dbc1f1abf1c92b5fde2a9620ba1ad33c1
Achrefgasri2 <gasriachref@gmail.com>
2021-12-02 18:53:07 +0100

    first commit

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -7,13 +7,9 @@ const MovieList = (props) => {
     return (
         <div className="listMovies">
              {props.movies
-              .filter(movie =>
-                {
-                    if(props.title === '' && props.rate === 0 ) return movie; 
-                    if(props.title !== '' && props.rate === 0 ) return movie.title.toUpperCase().includes(props.title.toUpperCase()); 
-                    if(props.title === '' && props.rate !== 0 ) return movie.rating.includes(props.rate); 
-                    if(movie.title.toUpperCase().includes(props.title.toUpperCase()) && movie.rating.includes(props.rate))return movie;
-                }).map((movie, i) =>
+              .filter(movie => movie.title.toUpperCase().includes(props.title.toUpperCase())  
+              && movie.rating >= props.rate
+                ).map((movie, i) =>
                 <div key={i}>
                     <MovieCard movie={movie} />
                 </div>
